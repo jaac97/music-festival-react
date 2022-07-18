940fe514b03a74c20c76baa6f1a0ce613ea8eeef
Josué Alarcón <josuealarco@gmail.com>
2022-07-18 12:01:52 -0500

    Finish hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,20 @@
 const Hero = () => {
     return (
-        <></>
+        <div className="hero">
+            <div className="hero__overlay">
+                <div className="hero__content hero__container">
+                    <h1 className="hero__heading">Rock & EDM Festival</h1>
+                    <p className="hero__subtitle">
+                        Julio 2022, Guadalajara, México. 
+                    </p>
+                </div>
+            </div>
+            <video className="videos" mute loop autoPlay>
+                <source src="video/concierto.mp4" type="video/mp4" />
+                <source src="video/concierto.ogg" type="video/ogg" />
+                <source src="video/concierto.webm" type="video/webm" />
+            </video>
+        </div>
     );
 }
  
