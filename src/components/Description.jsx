08d35694e7272b9db0648738f7a5d8803d4f4231
Josué Alarcón <josuealarco@gmail.com>
2022-07-18 12:00:51 -0500

    Add description section

---
+++ src/components/Description.jsx
@@ -0,0 +1,17 @@
+const Description = () => {
+    return ( 
+        <div className="festival festival__container" >
+            <div className="festival__image">
+            <img src="img/imagen_vocalista.jpg" alt="Vocalist image" />
+
+            </div>
+            <div className="festival__text">
+                <h2 className="festival__heading">Rock & EDM Festival</h2>
+                <p className="festival__subtitle">Julio 2022, Guadalajara, México</p>
+                <p className="festival__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor expedita nulla dignissimos repellendus corporis excepturi enim eum? Tenetur rerum excepturi natus dicta ullam sit, fuga asperiores maxime distinctio rem doloremque?</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Description;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './build/sass/app.scss';
+import Description from './components/Description';
 import Header from './components/Header'
 import Hero from './components/Hero';
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Header />
       <Hero />
+      <Description />
   </React.StrictMode>
 )
