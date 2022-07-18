af31b0ca0e799893475b8fc5bcd544314afaeb17
Josué Alarcón <josuealarco@gmail.com>
2022-07-18 14:09:31 -0500

    Add Images Avif, Webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 
-import gulp, {src, dest} from 'gulp';
+import gulp from 'gulp';
 import babel from 'gulp-babel';
+import webp from 'gulp-webp';
+import avif from 'gulp-avif';
 
-function tarea (done) {
-    console.log("Mi priemr tarea")
-    done()
+function convertWebp () {
+    return gulp.src('./src/build/img/**/*')
+    .pipe(webp())
+    .pipe(gulp.dest('./public/img'))
+}
+function convertAvif () {
+    return gulp.src('./src/build/img/**/*.{png,jpg,svg}')
+    .pipe(avif())
+    .pipe(gulp.dest('./public/img'))
 }
 
-export{
-    tarea
+function parallels (cb) {
+    gulp.parallel(convertWebp,convertAvif)
+    cb()
 }
+export default parallels
