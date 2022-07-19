8d2ed30fdcec2094dd4181fdf65c436e4dd65550
Josué Alarcón <josuealarco@gmail.com>
2022-07-19 10:31:20 -0500

    Add html

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -4,54 +4,54 @@ const Galery = () => {
             <h2 className="galery__heading">Galery</h2>
             <div className="photos">
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/1.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/1.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/1.jpg" lazyalt="thumb/1"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/1.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/1.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/1.jpg" lazyalt="thumb/1"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/2.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/2.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/2.jpg" lazyalt="thumb/2"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/2.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/2.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/2.jpg" lazyalt="thumb/2"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/3.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/3.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/3.jpg" lazyalt="thumb/3"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/3.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/3.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/3.jpg" lazyalt="thumb/3"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/4.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/4.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/4.jpg" lazyalt="thumb/4"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/4.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/4.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/4.jpg" lazyalt="thumb/4"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/5.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/5.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/5.jpg" lazyalt="thumb/5"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/5.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/5.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/5.jpg" lazyalt="thumb/5"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/6.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/6.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/6.jpg" lazyalt="thumb/6"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/6.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/6.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/6.jpg" lazyalt="thumb/6"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/7.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/7.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/7.jpg" lazyalt="thumb/7"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/7.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/7.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/7.jpg" lazyalt="thumb/7"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/8.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/8.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/8.jpg" lazyalt="thumb/8"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/8.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/8.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/8.jpg" lazyalt="thumb/8"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/9.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/9.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/9.jpg" lazyalt="thumb/9"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/9.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/9.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/9.jpg" lazyalt="thumb/9"/>
                 </picture>
                 <picture>
-                    <source media="(min-width: 600px)" srcset="img/thumb/10.avif" type="image/avif"/>
-                    <source media="(min-width: 600px)" srcset="img/thumb/10.webp" type="image/webp"/>
-                    <img loading="lazy" decoding="async" src="build/img/thumb/10.jpg" lazyalt="thumb/10"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/10.avif" type="image/avif"/>
+                    <source media="(min-width: 600px)" srcset="img/grande/10.webp" type="image/webp"/>
+                    <img loading="lazy" decoding="async" src="img/grande/10.jpg" lazyalt="thumb/10"/>
                 </picture>
             </div>
         </section>
